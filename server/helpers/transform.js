9f6fed1e17042fa86f88b23c601eb2dfe62d44b5
Angelo D'Autilia <angelo.dautilia@alcacoop.it>
2018-03-28 10:05:57 +0200

    Remove replace zoom to thumbnail

--- server/helpers/transform.js
+++ server/helpers/transform.js
@@ -8,7 +8,7 @@ module.exports = (data) => {
       "description": book.volumeInfo.description || "Description unavailable",
       "isbn": typeof book.volumeInfo.industryIdentifiers !== "undefined" ? book.volumeInfo.industryIdentifiers[0].identifier : "ISBN unavailable",
       "pages": book.volumeInfo.pageCount || "Page count unavailable",
-      "image": typeof book.volumeInfo.imageLinks !== "undefined" ? book.volumeInfo.imageLinks.thumbnail.replace(/zoom=1/, "zoom=2") : "Image unavailable"
+      "image": typeof book.volumeInfo.imageLinks !== "undefined" ? book.volumeInfo.imageLinks.thumbnail : "Image unavailable"
     }
   });
 }
