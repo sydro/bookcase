10a927cdf698d924de2a48c3b299090e5d26800f
Angelo D'Autilia <angelo.dautilia@alcacoop.it>
2018-03-30 14:51:20 +0200

    Added console.log for debug

--- server/apis/books.js
+++ server/apis/books.js
@@ -4,46 +4,45 @@ const https = require("https");
 const qs = require("../helpers/querystring");
 
 module.exports = {
-
   get: function(searchString) {
-
     var promise = new Promise((resolve, reject) => {
+      var urlParams = {
+        q: searchString
+      };
 
-        var urlParams = {
-          "q": searchString
-        };
+      var httpsOpts = {
+        hostname: "www.googleapis.com",
+        method: "GET",
+        path: "/books/v1/volumes?" + qs(urlParams)
+      };
+      var request = https.request(httpsOpts, response => {
+        var string = "";
 
-        var httpsOpts = {
-          "hostname": "www.googleapis.com",
-          "method": "GET",
-          "path": "/books/v1/volumes?" + qs(urlParams)
-        }
-        var request = https.request(httpsOpts, (response) => {
-
-          var string = "";
-
-          response.setEncoding("utf-8");
-          response.on("data", (chunk) => {
-            string += chunk;
-          });
-          response.on("end", () => {
-            try {
-              var jsonData = JSON.parse(string);
-            } catch (error) {
-              reject(error);
-            }
-            if (response.statusCode === 200) {
-              resolve(jsonData);
-            } else {
-              reject(jsonData);
-            }
-          });
+        response.setEncoding("utf-8");
+        response.on("data", chunk => {
+          string += chunk;
         });
-        request.on("error", (error) => {
-          reject(error);
+        response.on("end", () => {
+          try {
+            var jsonData = JSON.parse(string);
+          } catch (error) {
+            console.log(error);
+            reject(error);
+          }
+          if (response.statusCode === 200) {
+            resolve(jsonData);
+          } else {
+            console.log(jsonData);
+            reject(jsonData);
+          }
         });
-        request.end();
       });
-      return promise;
+      request.on("error", error => {
+        console.log(error);
+        reject(error);
+      });
+      request.end();
+    });
+    return promise;
   }
 };
